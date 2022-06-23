b08527a847efd3ed00ea1db7de4dabcb77ac91e3
shijingzhao <18632620921@163.com>
2022-06-23 17:06:24 +0800

    fix: 包引用 删除main引用

--- frame.go
+++ frame.go
@@ -1,21 +1,14 @@
 package main
 
-import (
-    "fmt"
-    "github.com/sjingzhao/go-frame/core"
-    "github.com/sjingzhao/go-frame/logic"
-    "time"
-)
-
 func main() {
-    fmt.Println(fmt.Sprintf("[%s] 服务启动", time.Now().Format("2006-01-02 15:04:05.000000")))
-    server := core.NewServer()
-    server.LoadService()
+    //fmt.Println(fmt.Sprintf("[%s] 服务启动", time.Now().Format("2006-01-02 15:04:05.000000")))
+    //server := core.NewServer()
+    //server.LoadService()
 
     // 注册路由
-    server.SetHttpServer(logic.RegisterRoute())
+    //server.SetHttpServer(logic.RegisterRoute())
     // 注册脚本
     //server.SetScriptServer(logic.RegisterScript())
 
-    server.Start()
+    //server.Start()
 }
